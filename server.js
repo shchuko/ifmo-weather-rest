503daa227dfaf32f90dbf9f06aa140563866b2ef
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-04-02 16:55:15 +0300

    server.js: minor fixes

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("db/config");
 const mongoose = require("mongoose");
-
+const config = require("./config");
 
 const app = express();
-const port = 8076;
+const port = config.port;
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.set("useCreateIndex", true);
 
 mongoose.connect(db.url, {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false})
     .then((mongoose) => {
