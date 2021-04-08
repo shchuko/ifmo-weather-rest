ca037949d4c3714f2b84c2a1849c4f23347e937c
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-04-09 00:12:29 +0300

    Move routes into one file

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,76 @@
-const weatherRoutes = require("./weather");
-const favouritesRoutes = require("./favourites");
+const asyncHandler = require("express-async-handler");
+const openWeatherMapRequests = require("../../datasources/openweathermap/requests");
 
-module.exports = function (app, mongoose) {
-    weatherRoutes(app);
-    favouritesRoutes(app, mongoose);
+module.exports = function (app, favouritesStorage) {
+    app.get("/weather/city", asyncHandler(async (req, res) => {
+        const cityName = req.query.q;
+
+        const json = await openWeatherMapRequests.requestWeatherByName(cityName);
+        if (json == null) {
+            res.status(404).send();
+            return;
+        }
+        res.send(json);
+    }));
+
+    app.get("/weather/coordinates", asyncHandler(async (req, res) => {
+        const lat = req.query.lat;
+        const lon = req.query.long;
+
+        const json = await openWeatherMapRequests.requestWeatherByLocation(lat, lon);
+        if (json == null) {
+            res.status(404).send();
+            return;
+        }
+        res.send(json);
+    }));
+
+    app.get("/favourites", asyncHandler(async (req, res) => {
+        res.send(await favouritesStorage.getAll());
+    }));
+
+    app.post("/favourites", asyncHandler(async (req, res) => {
+        const cityName = req.query.q;
+
+        if (cityName === undefined) {
+            res.status(400).send();
+            return;
+        }
+
+        let exists = await favouritesStorage.isContain(cityName);
+        if (exists) {
+            res.status(409).send();
+            return;
+        }
+
+        let json = await openWeatherMapRequests.requestWeatherByName(cityName);
+        if (json == null) {
+            res.status(404).send();
+            return;
+        }
+
+        exists = await favouritesStorage.isContain(json.name);
+        if (exists) {
+            res.status(409).send();
+            return;
+        }
+
+        res.status(201).send(json);
+        await favouritesStorage.addWithName(cityName);
+    }));
+
+    app.delete("/favourites", asyncHandler(async (req, res) => {
+        const cityName = req.query.q;
+
+        if (cityName === undefined) {
+            res.status(400).send();
+            return;
+        }
+
+        const result = await favouritesStorage.removeByName(cityName);
+        if (result !== true) {
+            res.status(404);
+        }
+        res.send();
+    }));
 };
